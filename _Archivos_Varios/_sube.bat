09baa8dca8d8e95fedaad113e155b26c80e477f4
Jesús Acosta <jesuseacosta@hotmail.com>
2022-02-19 23:18:24 +0100

    6ta. Actualización 19-Febrero-2022

--- _Archivos_Varios/_sube.bat
+++ _Archivos_Varios/_sube.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 git status
-git add  * --verbose
-git commit --message=%1% --verbose
+git add * --verbose
+git commit --message=%1% --verbose -a
 git log --oneline --since="30 minutes ago" --shortstat master
 git push --verbose
 git status
