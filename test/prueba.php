519618e00ba49d45aabc3a38bfa69971f25213d7
TSU Jes├║s E. Acosta M. (Churrique) <>
2024-02-05 18:05:35 +0100

    Modificaciones en el gitignore...

--- test/prueba.php
+++
@@ -1,5 +0,0 @@
-<?php
-  if (isset($_POST['enviar'])) {
-    echo '==>'.$_POST['copia'].'<===';
-  }
-  ?>
--- test/prueba_de_select.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once( '../scripts/php/variables.php' );
-include_once( '../scripts/php/basededatos.php' );
-include_once( '../scripts/php/funciones.php' );
-CrtSelect( 'datos_personales', 'sexo', 'secsec' );
-?>
